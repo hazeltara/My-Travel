0f20cc310bb8e4c288192796b3f6112b2cef872d
hazeltara <hazeltarazeereildaraharjo24@gmail.com>
2023-11-22 17:20:07 +0700

    Nambahin kata test di title

--- home.php
+++ home.php
@@ -7,7 +7,7 @@ include("config.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Travel</title>
+    <title>My Travel Test</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 
     <link rel="stylesheet"
